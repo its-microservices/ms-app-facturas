eed4adfab45ed2a0fc4defb3fb2e4802ee958542
DiLucaLucas <lucasmartindiluca@gmail.com>
2025-06-03 20:16:58 -0300

    `Feat: Prisma connection`

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FacturaModule } from './factura/factura.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [FacturaModule],
+  imports: [FacturaModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+
+  const logger = new Logger('MS-FACTURAS main');
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule, {
     transport: Transport.TCP,
@@ -12,9 +16,10 @@ async function bootstrap() {
       port: envs.PORT
     }
   }
-);
+  );
 
   await app.listen();
-  console.log('Factura microservice is running on port 4002');
+
+  logger.log(`Factura microservice is running on ${envs.DB_HOST}:${envs.PORT}`);
 }
 bootstrap();
