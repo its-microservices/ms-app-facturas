cc89e17311958674046aab4f2d4979b02a31921d
DiLucaLucas <lucasmartindiluca@gmail.com>
2025-06-30 09:05:10 -0300

    `Feat: Factura` | Add alta & listado de facturas. - Se integra conexion con mongo y prisma permitiendo el alta y listado.

--- src/factura/dto/create-factura.dto.ts
+++ src/factura/dto/create-factura.dto.ts
@@ -1,1 +1,19 @@
-export class CreateFacturaDto {}
+import { IsString, IsInt, IsNumber, IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { CreateItemDto } from './create-item.dto';
+
+export class CreateFacturaDto {
+    @IsInt()
+    numero: number;
+
+    @IsString()
+    cliente: string;
+
+    @IsNumber()
+    total: number;
+
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => CreateItemDto)
+    items: CreateItemDto[];
+}
--- src/factura/factura.service.ts
+++ src/factura/factura.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFacturaDto } from './dto/create-factura.dto';
 import { UpdateFacturaDto } from './dto/update-factura.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class FacturaService {
+
+  constructor(private _prisma: PrismaService) { }
+
   create(createFacturaDto: CreateFacturaDto) {
-    return 'This action adds a new factura';
+    return this._prisma.factura.create({
+      data: {
+        numero: createFacturaDto.numero,
+        cliente: createFacturaDto.cliente,
+        total: createFacturaDto.total,
+        items: {
+          create: createFacturaDto.items,
+        },
+      },
+      include: { items: true },
+    });
   }
 
   findAll() {
-    return `This action returns all factura`;
+    return this._prisma.factura.findMany({
+      include: {
+        items: true, // incluir los ítems relacionados
+      },
+    });
   }
 
   findOne(id: number) {
